12825e9de886a8b2fed625e7aa5489216156b640
OneWhoDevs <rohanravi183@gmail.com>
2023-03-14 00:40:27 +0530

    authentication middleware

--- backend/middleware/authenticate.js
+++ backend/middleware/authenticate.js
@@ -1,5 +1,28 @@
-const authenticate = ()=>{
+const jwt = require('jsonwebtoken');
+const User = require('../models/userSchema');
 
+const authenticate = async(req,res,next)=>{
+    try{
+        const token = req.cookies.mapgo;
+        const verify = jwt.verify(token, process.env.SECRET_KEY);
+
+        const rootUser = await User.findOne(
+            {_id: verify._id, "tokens:token": token}
+        )
+        
+        if(!rootUser){
+            throw new Error('User not found');
+        }
+
+        req.token = token;
+        req.rootUser = rootUser;
+        req.userId = rootUser._id;
+
+        next();
+    }
+    catch(err){
+        console.log(err);
+    }
 }
 
 module.exports = authenticate;
